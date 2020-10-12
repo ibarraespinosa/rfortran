0bd8a4ece47381b93c0487a1a0b13203ef86c2f7
ibarraespinosa <sergio.ibarra@usp.br>
2020-10-12 12:48:33 -0300

    add -cpp in , but it is not portable

--- src/get_threads.f90
+++ src/get_threads.f90
@@ -2,11 +2,13 @@ SUBROUTINE checkntf (nt) ! # nocov start
 USE OMP_LIB
 IMPLICIT NONE
 
+
 INTEGER nt
 
 #ifdef _OPENMP
 nt = OMP_GET_MAX_THREADS()
 #endif
+
 
 RETURN
 END ! # nocov end
--- src/init.c
+++ src/init.c
@@ -1,11 +1,10 @@
 #include <R_ext/RS.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
-#include "myomp.h"
 
 /* FIXME:
-   Check these declarations against the C/Fortran source code.
-*/
+ Check these declarations against the C/Fortran source code.
+ */
 
 /* .Fortran calls */
 extern void F77_NAME(base_types)(void *, void *, void *);
@@ -18,15 +17,16 @@ extern void F77_NAME(quadratic_pascal_reduced_dp)(void *, void *, void *, void *
 extern void F77_NAME(quadratic_reduced_dp)(void *, void *, void *, void *, void *, void *);
 
 static const R_FortranMethodDef FortranEntries[] = {
-    {"base_types", (DL_FUNC)&F77_NAME(base_types), 3},
-    {"cbrt_dp", (DL_FUNC)&F77_NAME(cbrt_dp), 3},
-    {"checkntf", (DL_FUNC)&F77_NAME(checkntf), 1},
-    {"lineq_gausselim_dp", (DL_FUNC)&F77_NAME(lineq_gausselim_dp), 5},
-    {"quadratic_dp", (DL_FUNC)&F77_NAME(quadratic_dp), 7},
-    {"quadratic_pascal_dp", (DL_FUNC)&F77_NAME(quadratic_pascal_dp), 7},
-    {"quadratic_pascal_reduced_dp", (DL_FUNC)&F77_NAME(quadratic_pascal_reduced_dp), 7},
-    {"quadratic_reduced_dp", (DL_FUNC)&F77_NAME(quadratic_reduced_dp), 6},
-    {NULL, NULL, 0}};
+    {"base_types",                  (DL_FUNC) &F77_NAME(base_types),                  3},
+    {"cbrt_dp",                     (DL_FUNC) &F77_NAME(cbrt_dp),                     3},
+    {"checkntf",                    (DL_FUNC) &F77_NAME(checkntf),                    1},
+    {"lineq_gausselim_dp",          (DL_FUNC) &F77_NAME(lineq_gausselim_dp),          5},
+    {"quadratic_dp",                (DL_FUNC) &F77_NAME(quadratic_dp),                7},
+    {"quadratic_pascal_dp",         (DL_FUNC) &F77_NAME(quadratic_pascal_dp),         7},
+    {"quadratic_pascal_reduced_dp", (DL_FUNC) &F77_NAME(quadratic_pascal_reduced_dp), 7},
+    {"quadratic_reduced_dp",        (DL_FUNC) &F77_NAME(quadratic_reduced_dp),        6},
+    {NULL, NULL, 0}
+};
 
 void R_init_rfortran(DllInfo *dll)
 {
