28c65a0492a652b70f3a3fce1674506adf8a7859
ibarraespinosa <sergio.ibarra@usp.br>
2020-12-04 15:23:04 -0300

    testing OMP

--- src/get_threads.f90
+++ src/get_threads.f90
@@ -1,12 +1,10 @@
 SUBROUTINE checkntf (nt) ! # nocov start
-USE OMP_LIB
+!$ USE OMP_LIB
 IMPLICIT NONE
 
 
 INTEGER nt
-#ifdef _OPENMP
 nt = OMP_GET_MAX_THREADS()
-#endif
 
 RETURN
 END ! # nocov end
